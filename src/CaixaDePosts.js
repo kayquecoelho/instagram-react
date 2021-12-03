51641b82b82908c66d317bd0b94a149f3645abd4
Kayque <lkcoelho76@gmail.com>
2021-12-03 13:18:17 -0300

    projeto 50%

---
+++ src/CaixaDePosts.js
@@ -0,0 +1,36 @@
+import Post from "./Post";
+
+const feedInfos = [
+  {
+    usuario: "meowed",
+    fotoDePerfil: "assets/img/meowed.svg",
+    conteudoImagem: "assets/img/gato-telefone.svg",
+    interacao: {
+      usuario: "respondeai",
+      fotoDePerfil: "assets/img/respondeai.svg",
+      numCurtidas: "101.523",
+    },
+  },
+  {
+    usuario: "barked",
+    fotoDePerfil: "assets/img/barked.svg",
+    conteudoImagem: "assets/img/dog.svg",
+    interacao: {
+      usuario: "adorable_animals",
+      fotoDePerfil: "assets/img/adorable_animals.svg",
+      numCurtidas: "99.159",
+    },
+  },
+];
+
+export default function Posts() {
+  return (
+    <div class="posts">
+      {feedInfos.map((item) => {
+        return <Post informacoes={item} />;
+      })}
+    </div>
+  );
+}
+
+
--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import ReactDOM from "react-dom";
 import Topo from "./Topo";
 import Corpo from "./Corpo";
 
-
-function App (){
-    return (
+function App() {
+  return (
     <div>
-        <Topo></Topo>
-        <Corpo></Corpo>
+      <Topo></Topo>
+      <Corpo></Corpo>
+      <div class="fundo-mobile">
+        <ion-icon name="home"></ion-icon>
+        <ion-icon name="search-outline"></ion-icon>
+        <ion-icon name="add-circle-outline"></ion-icon>
+        <ion-icon name="heart-outline"></ion-icon>
+        <ion-icon name="person-outline"></ion-icon>
+      </div>
     </div>
-    )
+  );
 }
 
-const root = document.querySelector(".root")
+const root = document.querySelector(".root");
 
-ReactDOM.render(<App/>, root)
+ReactDOM.render(<App />, root);
